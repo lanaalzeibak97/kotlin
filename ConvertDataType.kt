48730aad9dc0d91ab75ee014ba6f1cc654406f0c
Lana al-zeibak <20051429+lanaalzeibak97@users.noreply.github.com>
2017-09-29 23:00:41 -0700

    Create ConvertDataType.kt

---
+++ ConvertDataType.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>)
+{
+    var n1 :Int = 12
+    var s1 : String = n1.toString()
+    println(" n1 = "+ n1 + " S1 = " + s1 )
+
+    var n2 : Int = 1
+    var d2 : Double = n2.toDouble()
+    println(" n2 = "+ n2 + " d2 = " + d2 )
+
+    var  s3 : String = "12"
+    var n3 : Int = s3.toInt()
+    println(" s3 = "+ s3 + " n3 = " + n3 )
+
+    var n4 : Int = 12
+    var f4 : Float = n4.toFloat() // f4 = 12.4f For float we add f to number
+    println(" n4 = "+ n4 + " f4 = " + f4 )
+
+}
